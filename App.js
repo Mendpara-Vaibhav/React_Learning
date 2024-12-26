28d2ffa9adbc1b92f334cfa6af82310903699c98
Mendpara Vaibhav <mendparavaibhav@gmail.com>
2024-12-26 13:36:21 +0530

    Ep-04 half

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => (
-  <h1 className="head" tabIndex="5">
-    React Learning using JSX
-  </h1>
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?size=large"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-const HeadingComponent = () => (
-  <div id="container">
-    {Title()}
-    <Title />
-    <Title></Title>
-    <h1 className="heading">React Learning Functional Component</h1>
-  </div>
-);
+const RestaurantCard = () => {
+  return (
+    <div
+      className="res-card"
+      style={{
+        backgroundColor: "#f0f0f0",
+      }}
+    >
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_300/FOOD_CATALOG/IMAGES/CMS/2024/5/6/42adbd33-843a-48e8-ad47-155f16de7d36_0ef72bc7-13da-4628-9ebc-04aafbf7537b.jpg"
+      />
+      <h3>Meghana Foods</h3>
+      <h4>Veg Biryani, North Indian, Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 min</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
